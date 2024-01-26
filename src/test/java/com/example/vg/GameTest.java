8a35476ff48614d79bb6f794a764b6ece5c9c40a
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2024-01-26 13:22:15 +0100

    On two rolls return som of this rolls

--- src/test/java/com/example/vg/GameTest.java
+++ src/test/java/com/example/vg/GameTest.java
@@ -15,4 +15,12 @@ class GameTest {
         assertThat(game.score()).as("Return 10").isEqualTo(10);
     }
 
+    @Test
+    @DisplayName("On two rolls return sum of this rolls")
+    void onTwoRollsReturnSumOfThisRolls() {
+        game.roll(3);
+        game.roll(2);
+
+        assertThat(game.score()).as("Return 5").isEqualTo(5);
+    }
 }
