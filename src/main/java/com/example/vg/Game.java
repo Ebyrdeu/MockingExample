3424017e9f49e2692296aca21fe028b11ec91d5e
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2024-01-27 11:54:25 +0100

    Assure that full game is wokring

--- src/main/java/com/example/vg/Game.java
+++ src/main/java/com/example/vg/Game.java
@@ -1,7 +1,6 @@
 package com.example.vg;
 
 public class Game implements Gaming {
-    private final int FRAMES = 10;
     private final int[] totalRolls = new int[21];
     private int currentRoll = 0;
 
@@ -14,30 +13,41 @@ public class Game implements Gaming {
 
     @Override
     public int score() {
-        int frame = 0;
-        for (int roll : totalRolls) {
-            if (roll < 0) throw new IllegalArgumentException("Negatives not allowed");
-            if (roll > 10) throw new IllegalArgumentException("More than 10 pins per roll not allowed");
-        }
+        int activeRoll = 0;
+        int frames = 10;
 
-        for (int i = 0; i < FRAMES; i++) {
-            if (isStrike(frame)) {
-                currentScore += 10 + strikeBonus(frame);
-                frame++;
-            }
-            else {
-                currentScore += totalRolls[frame];
-                frame++;
+        for (int i = 0; i < frames; i++) {
+            if (totalRolls[activeRoll] > 10) throw new IllegalArgumentException("More than 10 pins per roll not allowed");
+            if (totalRolls[activeRoll] < 0) throw new IllegalArgumentException("Negatives not allowed");
+
+            if (isStrike(activeRoll)) {
+                currentScore += 10 + strikeBonus(activeRoll);
+                activeRoll++;
+            } else if (isSpare(activeRoll)) {
+                currentScore += 10 + spareBonus(activeRoll);
+                activeRoll += 2;
+            } else {
+                currentScore += totalRolls[activeRoll] + totalRolls[activeRoll + 1];
+                activeRoll += 2;
             }
         }
-
 
         return currentScore;
     }
-    private int strikeBonus(int frame) {
-        return totalRolls[frame + 1] + totalRolls[frame + 2];
+
+    private int strikeBonus(int activeRoll) {
+        return totalRolls[activeRoll + 1] + totalRolls[activeRoll + 2];
     }
-    private boolean isStrike(int frame) {
-        return totalRolls[frame] == 10;
+
+    private boolean isStrike(int activeRoll) {
+        return totalRolls[activeRoll] == 10;
+    }
+
+    private boolean isSpare(int activeRoll) {
+        return totalRolls[activeRoll] + totalRolls[activeRoll + 1] == 10;
+    }
+
+    private int spareBonus(int activeRoll) {
+        return totalRolls[activeRoll + 2];
     }
 }
