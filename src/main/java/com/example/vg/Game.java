b014a89f606639762877a2ce4fa79525760def8f
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2024-01-27 10:50:34 +0100

    assure strike on 10 pinns

--- src/main/java/com/example/vg/Game.java
+++ src/main/java/com/example/vg/Game.java
@@ -2,7 +2,7 @@ package com.example.vg;
 
 public class Game implements Gaming {
     private final int FRAMES = 10;
-    private final int[] totalRolls = new int[21] ;
+    private final int[] totalRolls = new int[21];
     private int currentRoll = 0;
 
     private int currentScore = 0;
@@ -14,13 +14,30 @@ public class Game implements Gaming {
 
     @Override
     public int score() {
+        int frame = 0;
         for (int roll : totalRolls) {
-            if (roll < 0 ) throw new IllegalArgumentException("Negatives not allowed");
-            if (roll > 10) throw  new IllegalArgumentException("More than 10 pins per roll not allowed");
-            currentScore += roll;
+            if (roll < 0) throw new IllegalArgumentException("Negatives not allowed");
+            if (roll > 10) throw new IllegalArgumentException("More than 10 pins per roll not allowed");
         }
+
+        for (int i = 0; i < FRAMES; i++) {
+            if (isStrike(frame)) {
+                currentScore += 10 + strikeBonus(frame);
+                frame++;
+            }
+            else {
+                currentScore += totalRolls[frame];
+                frame++;
+            }
+        }
+
 
         return currentScore;
     }
-
+    private int strikeBonus(int frame) {
+        return totalRolls[frame + 1] + totalRolls[frame + 2];
+    }
+    private boolean isStrike(int frame) {
+        return totalRolls[frame] == 10;
+    }
 }
--- src/test/java/com/example/vg/GameTest.java
+++ src/test/java/com/example/vg/GameTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameTest {
-    private Game game ;
+    private Game game;
 
     @BeforeEach()
     void init() {
@@ -40,11 +40,31 @@ class GameTest {
 
         assertThatThrownBy(game::score).isInstanceOf(IllegalArgumentException.class).hasMessage("Negatives not allowed");
     }
-@Test
-@DisplayName("Throw exception when points execl 10")
-void throwExceptionWhenPointsExecl10() {
-    game.roll(11);
 
-    assertThatThrownBy(game::score).isInstanceOf(IllegalArgumentException.class).hasMessage("More than 10 pins per roll not allowed");
-}
+    @Test
+    @DisplayName("Throw exception when points execl 10")
+    void throwExceptionWhenPointsExecl10() {
+        game.roll(11);
+
+        assertThatThrownBy(game::score).isInstanceOf(IllegalArgumentException.class).hasMessage("More than 10 pins per roll not allowed");
+    }
+
+    @Test
+    @DisplayName("on 10 Pinns assure it a strike")
+    void on10PinnsAssureItAStrike() {
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+
+        assertThat(game.score()).isEqualTo(300);
+    }
 }
